cffcba83a599d2580a9823c20f8a53f692fb74bd
Nicolas de Paula Lanca - PrestServ <nicolas.lanca.prestserv@petrobras.com.br>
2025-12-22 13:40:07 -0300

    Atualizaçoes

--- Site/settings.py
+++ Site/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
      BASE_DIR / '.loja/static/',
     
 ]
-
+STATIC_ROOT=BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
