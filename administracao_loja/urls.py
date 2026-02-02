d9af9f4050f6b5221693b0fac3a296983f4d5953
Nicolas de Paula Lanca - PrestServ <nicolas.lanca.prestserv@petrobras.com.br>
2026-02-02 13:41:29 -0300

    Criando administacao 0.1

--- administracao_loja/urls.py
+++ administracao_loja/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from administracao_loja import views
 urlpatterns = [
-    path('',views.painel_gestao,name="index")
+    path('',views.painel_gestao,name="index"),
+    path('usuarios',views.Usuarios,name="usuarios"),
+    path('pedidos',views.pedidos,name="pedidos")
 ]
--- administracao_loja/views.py
+++ administracao_loja/views.py
@@ -6,9 +6,15 @@ from loja.models import Usuario, Pedido
 
 def painel_gestao(request):
     usuarios = Usuario.objects.all()
-    pedidos = Pedido.objects.select_related('cliente').order_by('-data_pedido')
-
+    
     return render(request, './administracao_loja/static/html/index.html', {
         'usuarios': usuarios,
-        'pedidos': pedidos
+        
     })
+def Usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, './administracao_loja/static/html/usuarios.html',{
+        'usuarios': usuarios})
+def pedidos(request):
+    pedidos = Pedido.objects.select_related('cliente').order_by('-data_pedido')
+    return render(request, './administracao_loja/static/html/pedidos.html', {'pedidos': pedidos})
