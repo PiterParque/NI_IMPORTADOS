91412500b9c3ba963ee09ffec0b77d50d3669c4f
Nicolas de Paula Lanca - PrestServ <nicolas.lanca.prestserv@petrobras.com.br>
2026-02-11 08:56:22 -0300

    consertando vercel

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@
 import os
 import sys
 
-
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+app=application
 if 'uuid' in sys.modules:
     del sys.modules['uuid']
 
